57cb227d6df937a15426fd516f3a4cd9be5c3a69
Jola Gryko <jgryko@gmail.com>
2023-09-11 12:44:03 +0200

    js

---
+++ js/podstawy/03_opcje.js
@@ -0,0 +1,22 @@
+const mezczyznaE = document.querySelector('#mezczyzna');
+const kobietaE = document.querySelector('#kobieta');
+const buttonE = document.querySelector('button');
+const wynikE = document.querySelector('#wynik');
+
+buttonE.addEventListener('click',(event)=>{
+    event.preventDefault();
+    console.log(kobietaE)
+    // if(kobietaE.checked){
+    //     wynikE.textContent="kobieta"
+    // }
+    // else{
+    //     wynikE.textContent="mezczyzna"
+    // }
+    if(kobietaE.checked){
+        wynikE.textContent=kobietaE.value
+    }
+    else{
+        wynikE.textContent=mezczyznaE.value
+    }
+    
+})

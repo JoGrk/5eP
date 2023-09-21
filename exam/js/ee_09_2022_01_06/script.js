4d0fd1debb79bc53918303cc8e2fd5e010c29675
Jola Gryko <jgryko@gmail.com>
2023-09-21 14:08:52 +0200

    exam

--- exam/js/ee_09_2022_01_06/script.js
+++ exam/js/ee_09_2022_01_06/script.js
@@ -3,6 +3,11 @@ const steelblueE = document.querySelector('#steelblue');
 const oliveE = document.querySelector('#olive');
 const fontColorE = document.querySelector('#font-color');
 const rightE=document.querySelector('#right');
+const sizeE=document.querySelector('#size');
+const borderE = document.querySelector('#border');
+const pictureE = document.querySelector('#picture');
+const listE = document.querySelector('#right ul');
+
 
 indigoE.addEventListener('click', (e)=>{
     rightE.style.backgroundColor='indigo';
@@ -16,6 +21,20 @@ oliveE.addEventListener('click', (e)=>{
 fontColorE.addEventListener('change', (e)=>{
     rightE.style.color=fontColorE.value
 })
+sizeE.addEventListener('blur',(e)=>{
+    rightE.style.fontSize=`${sizeE.value}%`
+})
 
+borderE.addEventListener('change',(e)=>{
+    if(borderE.checked){
+        pictureE.style.border='solid white 1px';
+    }else{
+        pictureE.style.border='none';
+    }
+});
 
-
+radio_div.addEventListener('change', (event)=>{
+    const nameRadio = document.querySelector(`input[name='bullet']:checked`);
+    listE.style.listStyleType = nameRadio.value;
+    
+})

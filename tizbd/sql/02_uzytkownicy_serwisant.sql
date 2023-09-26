c5866aaad3db659f79903b0d7884f797916651af
Jola Gryko <jgryko@gmail.com>
2023-09-26 08:22:10 +0200

    sql

--- tizbd/sql/02_uzytkownicy_serwisant.sql
+++ tizbd/sql/02_uzytkownicy_serwisant.sql
@@ -32,34 +32,55 @@ ALTER USER 'kupiec'@'localhost' PASSWORD EXPIRE INTERVAL 1 DAY;
 -- A. bazę danych Egzaminy
 -- B. w tej bazie tabelę Test (pole id typu int, klucz podstawowy, bez autoinkrementacji). 
 -- C. Dodaj do tabeli Test cyfry 1, 2 i 3 
+
 -- D. Utwórz tabelę tabela z jednym polem id typu całkowitego.
- CREATE DATABASE Egzaminy
+ CREATE DATABASE Egzaminy;
 use Egzaminy;
 
 CREATE TABLE test(
     id INT PRIMARY KEY
-)
- INSERT INTO Test 
+);
+
+ INSERT INTO Test
  VALUES 
- (1,2,3)
+ (1),(2),(3);
+
+ CREATE TABLE tabela(
+    id INT
+ );
 -- 10. Daj kupcowi prawo do wprowadzania danych do tabeli test w bazie Egzaminy (samo wprowadzanie, bez wyszukiwania)
  
+GRANT INSERT ON Egzaminy.test TO 'kupiec'@'localhost'; 
+ 
 -- 11. Daj szukaczowi prawo do wyświetlania danych z tabeli test w bazie Egzaminy
- 
+ GRANT SELECT ON Egzaminy.test TO 'szukacz'@'localhost';
 -- 12. Sprawdź, czy kupiec (prawo do wprowadzania danych):
-
+ SET PASSWORD = PASSWORD('5678');
 -- A. widzi bazę Egzaminy
+SHOW DATABASES;
 -- B. Może jej użyć
+use egzaminy;
 -- C. Widzi tabele w tej bazie
+show tables;
 -- D. Może wyświetlić zawartość tabeli Test
+SELECT *
+from test;
 -- E. Może dodać dane (liczbę 4)
- 
+ INSERT INTO Test
+ VALUES 
+ (4);
 -- 13. Sprawdź, czy szukacz (prawo do wyświetlania danych):
 -- A. widzi bazę Egzaminy
+SHOW DATABASES;
 -- B. Może jej użyć
+ use egzaminy;
 -- C. Widzi tabele w tej bazie
+SHOW tables;
 -- D. Może wyświetlić zawartość tabeli Test
+SELECT * FROM test;
 -- E. Może dodać dane (liczbę 5)
+INSERT INTO test 
+VALUES (5);
 -- 14. Utwórz użytkownika serwisant
  
 -- 15. Daj prawo serwisantowi do usuwania danych z tabeli test w bazie egzaminy (samo usuwanie, bez prawa do wyszukiwania).

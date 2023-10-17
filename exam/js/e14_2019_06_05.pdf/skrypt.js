ff38ebe2c697b1cfbeddbc28fbfc765c0505b025
Jola Gryko <jgryko@gmail.com>
2023-10-17 08:37:34 +0200

    ex

---
+++ exam/js/e14_2019_06_05.pdf/skrypt.js
@@ -0,0 +1,9 @@
+const nameE = document.querySelector('#name')
+const surnameE = document.querySelector('#surname')
+const emailE = document.querySelector('#email')
+const serviceE = document.querySelector('#service')
+const checkE = document.querySelector('#check')
+send.addEventListener('click',(e)=>{
+    e.preventDefault();
+    
+})

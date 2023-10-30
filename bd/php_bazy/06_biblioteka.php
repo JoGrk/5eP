d9c94ba529c2bbb031e2c4409eeb119bad87a2c7
Jola Gryko <jgryko@gmail.com>
2023-10-30 11:36:08 +0100

    php_sql

--- bd/php_bazy/06_biblioteka.php
+++ bd/php_bazy/06_biblioteka.php
@@ -8,10 +8,42 @@
 </head>
 <body>
     <form action="06_biblioteka.php" method="POST">
-        
+        <?php
+    $link = new mysqli('localhost', 'root', '', 'ex_biblioteka');
+    $sql = "select id, nazwa from wydawnictwa";
+    $result = $link->query($sql);
+    while($row = $result->fetch_assoc()){
+        $id = $row['id'];
+        $nazwa = $row['nazwa'];
+        echo "<input type='radio' name='wydawnictwo' id='w.$id' value='$id'>";
+        echo "<label for='w.$id'>$nazwa</label>";
+        echo "<br>";
+    }
+    $link->close();
+        ?>
+  
+        <button>Wyślij</button>
     </form>
     <?php
-    
+        if (!isset($_POST['wydawnictwo'])){
+            echo "wybierz wydawnictwo";
+        }
+        else {
+
+            $id_wydawnictwo = $_POST['wydawnictwo']; 
+            $link = new mysqli('localhost','root','','ex_biblioteka');
+            $sql="SELECT tytul from ksiazki WHERE id_wydawnictwo=$id_wydawnictwo;";
+            $result=$link->query($sql);
+            echo "<ul>";
+            while($row=$result->fetch_assoc()){
+                $tytul = $row['tytul'];
+                echo "<li>$tytul</li>"; 
+            }
+            echo "</ul>";
+
+            $link->close();
+        }
+        
     ?>
 </body>
 </html>

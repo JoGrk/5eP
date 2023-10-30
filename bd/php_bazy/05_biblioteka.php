6b87510103ca34263dc119901221d6b58f69ed87
Jola Gryko <jgryko@gmail.com>
2023-10-30 10:31:53 +0100

    php_sql

--- bd/php_bazy/05_biblioteka.php
+++ bd/php_bazy/05_biblioteka.php
@@ -15,18 +15,29 @@
         <input type="reset" value="Reset">
     </form>
     <?php
+
+        $link = new mysqli ('localhost','root','','ex_biblioteka');
     if(empty($_POST['firstname']) || empty($_POST['surname'])){
         echo "uzupełnij dane";
     }
     else{
         $firstname = $_POST['firstname']; 
         $surname = $_POST['surname'];
-        echo "$firstname $surname";
-        $sql="SELECT tytul, autorzy.imie, autorzy.nazwisko FROM ksiazki INNER JOIN autorzy ON autorzy.id=ksiazki.id_autor INNER JOIN wypozyczenia ON ksiazki.id=wypozyczenia.id_ksiazka INNER JOIN czytelnicy ON wypozyczenia.id_czytelnik=czytelnicy.id WHERE czytelnicy.imie='$firstname' AND czytelnicy.nazwisko='$surname';"
+        // echo "$firstname $surname";
+        $sql="SELECT tytul, autorzy.imie, autorzy.nazwisko FROM ksiazki INNER JOIN autorzy ON autorzy.id=ksiazki.id_autor INNER JOIN wypozyczenia ON ksiazki.id=wypozyczenia.id_ksiazka INNER JOIN czytelnicy ON wypozyczenia.id_czytelnik=czytelnicy.id WHERE czytelnicy.imie='$firstname' AND czytelnicy.nazwisko='$surname'";
+        
+        $result = $link -> query($sql);
+        while($row=$result->fetch_assoc()){
+            $tytul = $row['tytul'];
+            $imie = $row['imie'];
+            $nazwisko = $row['nazwisko']; 
+            echo "<p>$tytul $imie $nazwisko</p>";
+        }
+       
     }
 
 
-
+    $link -> close();
 
     ?>
     

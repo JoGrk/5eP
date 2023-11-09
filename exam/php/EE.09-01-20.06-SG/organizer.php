82e83398912ed6c292f408350016766a26999837
Jola Gryko <jgryko@gmail.com>
2023-11-09 13:20:37 +0100

    ex_organizer

---
+++ exam/php/EE.09-01-20.06-SG/organizer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl6.css">
+    <title>Organizer</title>
+</head>
+<body>
+    <header>
+        <section id='baner1'>
+            <h2>MÓJ ORGANIZER</h2>
+        </section>
+        <section id='baner2'>
+            <form action="organizer.php" method="POST">
+                <label for="wpis">Wpis wydarzenia</label>
+                <input type="text" name="wpis" id="wpis">
+                <button>ZAPISZ</button>
+            </form>
+        </section>
+        <section id='baner3'>
+            <a href="logo2.png" alt="Mój organizer"></a>
+        </section>
+    </header>
+
+    <main>
+        <!-- skrypt1 -->
+    </main>
+
+    <footer> 
+        <!-- skrypt2 -->
+        <p>Stronę wykonał:</p>
+    </footer>
+</body>
+</html>

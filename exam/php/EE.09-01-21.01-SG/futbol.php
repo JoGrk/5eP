56a911d93a90e3c4d10a327119bca0a16ec37cec
Jola Gryko <jgryko@gmail.com>
2023-11-13 11:03:15 +0100

    ex_php

---
+++ exam/php/EE.09-01-21.01-SG/futbol.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rozgrywki futbolowe</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+
+    <header>
+        <h2>Światowe rozgrywki piłkarskie</h2>
+        <img src="obraz.jpg" alt="boisko">
+    </header>
+
+    <section id="mecze">
+        <!-- Fajny Skrypt1 -->
+    </section>
+
+    <main>
+        <h2>Reprezentacja Polski</h2>
+    </main>
+
+    <footer>
+        <section id="lewy">
+            <p>Podaj pozycję zawodników (1-bramkarze, 2-obrońcy, 3-pomocnicy, 4-napastnicy)</p>
+
+            <form action="futbol.php" method="post">
+                <input type="number" name="pozycja" id="pozycja">
+                <button>Sprawdź</button>
+            </form>
+            <ul>
+                <!-- skrypt2 -->
+            </ul>
+        </section>
+        <section id="prawy">
+            <img src="zad1.png" alt="piłkarz">
+            <p>Autor:000000000000000</p>
+        </section>
+    </footer>
+
+    
+</body>
+</html>

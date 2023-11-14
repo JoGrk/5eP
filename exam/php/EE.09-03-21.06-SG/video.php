a855607c286b0493ad51fb1e50a7a5ce72d0ddb9
Jola Gryko <jgryko@gmail.com>
2023-11-14 12:05:14 +0100

    e_php

---
+++ exam/php/EE.09-03-21.06-SG/video.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl3.css">
+    <title>Video On Demand</title>
+</head>
+<body>
+    <header>
+        <section id='left'>
+            <h1>Internetowa wypozyczalnia filmów</h1>
+        </section>
+        <section id='right'>
+            <table>
+                <tr>
+                    <td>Kryminał</td>
+                    <td>Horror</td>
+                    <td>Przygodowy</td>
+                </tr>
+                <tr>
+                    <td>20</td>
+                    <td>30</td>
+                    <td>20</td>
+                </tr>
+            </table>
+        </section>
+    </header>
+    <main>
+        <section class='list-film'>
+            <h3>Polecamy</h3>
+            <div class="flex">
+                <!-- skrypt 1 -->
+            </div>
+        </section>
+        <section class='list-film'>
+            <h3>Filmy fantastyczne</h3>
+            <div class="flex">
+                <!-- skrypt 2 -->
+            </div>
+        </section>
+    </main>
+    <footer>
+        <form action="video.php" method="POST">
+        <label for="delete-film">Usuń film nr:</label>    
+        <input type="number" name="delete-film" id="delete-film">
+        <button>Usuń film</button>
+        <!-- skrypt3 -->
+        <p>Stronę wykonał: 12345678911</p>
+        </form>
+
+    </footer>
+</body>
+</html>
--- exam/php/EE.09-03-22.06-SG/opinie.php
+++ exam/php/EE.09-03-22.06-SG/opinie.php
@@ -41,6 +41,19 @@
             <h3>Nasi top klienci</h3>
             <ul >
                 <!-- tutaj jest skrypt2 -->
+                <?php
+                $link = new mysqli('localhost', 'root', '', 'ex_hurtownia');
+                $sql = "SELECT imie,nazwisko,punkty from klienci order by punkty desc LIMIT 3";
+                $result = $link->query($sql);
+                while($row = $result ->fetch_assoc()){
+                    $imie =$row['imie'];
+                    $nazwisko =$row['nazwisko'];
+                    $punkty =$row['punkty'];
+                    echo "<li>$imie $nazwisko, $punkty pkt</li>";
+                    
+                }
+                $link->close();
+                ?>
             </ul>
         </section>
         <section >

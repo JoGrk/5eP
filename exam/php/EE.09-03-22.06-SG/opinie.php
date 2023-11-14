a969a77f7b56f661fc6a4ae7db242c3597c8a125
Jola Gryko <jgryko@gmail.com>
2023-11-14 08:11:05 +0100

    ex_php

--- exam/php/EE.09-03-22.06-SG/opinie.php
+++ exam/php/EE.09-03-22.06-SG/opinie.php
@@ -11,26 +11,28 @@
     <header>
         <h1>Hurtownia spożywcza</h1>
     </header>
+
     <main>
         <h2>Opinie naszych klientów</h2>
         <!-- tutaj jest skrypt1 -->
     </main>
-    <footer>
-        <section id="one">
+
+    <footer >
+        <section >
             <h3>Współpracują z nami</h3>
             <a href="http://sklep.pl/">Sklep 1</a>
         </section>
-        <section id="two">
+        <section >
             <h3>Nasi top klienci</h3>
-            <ul>
+            <ul >
                 <!-- tutaj jest skrypt2 -->
             </ul>
         </section>
-        <section id="three">
+        <section >
             <h3>Skontaktuj się</h3>
             <p>telefon: 997 997 997</p>
         </section>
-        <section id="four">
+        <section >
             <h3>autor: 000000000000</h3>
         </section>
     </footer>

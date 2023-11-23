f0692971d6068e71bf5ed92a4b37590fda41a5e7
Jola Gryko <jgryko@gmail.com>
2023-11-23 13:35:12 +0100

    szkola

---
+++ exam/php/przyloty - EE.09-05-21.01-SG/lotnisko.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Port Lotniczy</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+    <header>
+        <section id="b1">
+            <img src="zad5.png" alt="logo lotnisko">
+        </section>
+        <section id="b2">
+            <h1>Przyloty</h1>
+        </section>
+        <section id="b3">
+            <h3>Przydatne linki</h3>
+            <a href="kwerendy.txt" target="_blank">Pobierz ...</a>
+        </section>
+    </header>
+
+    <main></main>
+
+    <footer>
+        <section id="f1">
+
+        </section>
+        <section id="f2">
+
+        </section>
+    </footer>
+    
+</body>
+</html>

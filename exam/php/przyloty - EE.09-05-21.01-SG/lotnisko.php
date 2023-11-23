4ada563dfa0e78eafb0ef720870d5ef38d18a52c
Jola Gryko <jgryko@gmail.com>
2023-11-23 14:09:09 +0100

    przyloty

--- exam/php/przyloty - EE.09-05-21.01-SG/lotnisko.php
+++ exam/php/przyloty - EE.09-05-21.01-SG/lotnisko.php
@@ -29,15 +29,37 @@
                 <th>numer rejsu</th>
                 <th>status</th>
             </tr>
+            <!-- skrypt 1 -->
+        <?php
+        $link = new mysqli('localhost', 'root', '', 'ex_przyloty');
+        $sql = "SELECT czas, kierunek, nr_rejsu, status_lotu FROM przyloty ORDER BY czas";
+        $result=$link->query($sql);
+        while($row=$result->fetch_assoc()){
+            echo "<tr>";
+            foreach($row as $field){
+                echo "<td>$field</td>";
+            }
+            echo "</tr>";
+        }
+        $link->close();
+        ?>
         </table>
     </main>
 
     <footer>
         <section id="f1">
-
+            <!-- skrypt 2 -->
+            <?php
+            if(!isset($_COOKIE['ciasteczko'])){
+                setcookie('ciasteczko','smaczne',time()+60*60*2);
+                echo "<p><b>Dzień dobry! Strona lotniska używa ciasteczek</b></p>";
+            }else{
+                echo "<p><i>Witaj ponownie na stronie lotniska</i></p>";
+            }
+            ?>  
         </section>
         <section id="f2">
-
+            Autor:.....
         </section>
     </footer>
     

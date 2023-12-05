e556499f44de29e7d7471dca6cbeed077c55f596
Jola Gryko <jgryko@gmail.com>
2023-12-05 08:35:10 +0100

    ex_php

---
+++ exam/php/portal - EE.09-05-22.01-SG/uzytkownicy.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal spoełcznościowy</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+    <header>
+        <div id='left-header'>
+            <h2>Nasze osiedle</h2>
+        </div>
+        <div id='right-header'>
+            <!-- skrypt1 -->
+        </div>
+    </header>
+    <main>
+        <div id='left'>
+            <h3>Logowanie</h3>
+            <form action="uzytkownicy.php" method="post">
+                login 
+                <br>
+                <input type="text" name="login">
+                <br>
+                hasło
+                <br>
+                <input type="password" name="password">
+                <button>Zaloguj</button>
+            </form>
+            
+        </div>
+        <div id='right'>
+            <h3>Wizytówka</h3>
+            <section>
+                <?php
+                // skrypt 2 
+                ?>
+            </section>
+
+
+        </div>
+    </main>
+    <footer>
+        <p>Stronę wykonał: 000000000</p>
+
+    </footer>
+</body>
+</html>
